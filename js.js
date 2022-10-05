19279fa4ee8bc156dddcf82840bffad95a8d90a1
Wojciach <wojciech.wyroslak@gmail.com>
2022-10-05 22:25:14 +0100

    added test.html to test fetch instead of xmlhttprequest, changed js.js search function to search in every column

--- js.js
+++ js.js
@@ -23,23 +23,28 @@ function includeHTML() {
         /* Exit the function: */
         return;
       }
-    }
+    } 
   }
 
   function myFunction() {
     // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, table, tr, td1, td2, td3, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("customers");
     tr = table.getElementsByTagName("tr");
   
     // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    for (i = 1; i < tr.length; i++) {
+      td1 = tr[i].getElementsByTagName("td")[0];
+      td2 = tr[i].getElementsByTagName("td")[1];
+      td3 = tr[i].getElementsByTagName("td")[2]; 
+
+       tdText = td1.innerText + td2.innerText + td3.innerText;
+
+      if (tdText) {
+        
+        if (tdText.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
         } else {
           tr[i].style.display = "none";
